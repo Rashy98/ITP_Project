1e399803ad9a42143a8a1953fb997dff628f0313
Nippa07 <nip.manoratne@gmail.com>
2019-08-27 14:00:18 +0530

    exp & financedetails

---
+++ ITP_group09_09/Expenses.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Expenses
+{
+	public Expenses()
+	{
+        public int id { get; set; }
+        public float Emp_salary { get; set; }
+        public float ProductionCost { get; set; }
+        public float MaintanenceCost { get; set; }
+        public float UtilityCost { get; set; }
+    }
+}
---
+++ ITP_group09_09/Finance_Details.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Finance_Details
+{
+    public Finance_Details()
+    { 
+        public int Id { get; set; }
+        public String Month { get; set; }
+        public float TotalExpenses { get; set; }
+        public float Income { get; set; }
+        public float MonthlyProfit { get; set; }
+    }
+}
